108407633b93f30f789a217226b88652d32ac682
Taku Miyamoto <40813148+miyamoto83@users.noreply.github.com>
2024-09-13 13:54:03 +0900

    pytest: Made is_folder_mock a public method.

--- osfclient/tests/test_cloning.py
+++ osfclient/tests/test_cloning.py
@@ -9,9 +9,7 @@ from osfclient.cli import clone
 
 from osfclient.tests.mocks import MockProject
 from osfclient.tests.mocks import MockArgs
-
-def _is_folder_mock(file_or_folder):
-    return file_or_folder._mock_name.startswith('Folder-')
+from osfclient.tests.mocks import is_folder_mock
 
 @patch.object(OSF, 'project', return_value=MockProject('1234'))
 def test_clone_project(OSF_project):
@@ -23,7 +21,7 @@ def test_clone_project(OSF_project):
     with patch('osfclient.cli.open', mock_open_func):
         with patch('osfclient.cli.makedirs'):
             with patch('osfclient.cli.os.getenv', side_effect='SECRET'):
-                with patch('osfclient.cli.is_folder', side_effect=_is_folder_mock):
+                with patch('osfclient.cli.is_folder', side_effect=is_folder_mock):
                     clone(args)
 
     OSF_project.assert_called_once_with('1234')
@@ -63,7 +61,7 @@ def test_clone_project_update_file_exists_and_matches(OSF_project, checksum):
         with patch('osfclient.cli.makedirs'):
             with patch('osfclient.cli.os.getenv', side_effect='SECRET'):
                 with patch('osfclient.cli.os.path.exists', side_effect=exists):
-                    with patch('osfclient.cli.is_folder', side_effect=_is_folder_mock):
+                    with patch('osfclient.cli.is_folder', side_effect=is_folder_mock):
                         clone(args)
 
     OSF_project.assert_called_once_with('1234')
@@ -106,7 +104,7 @@ def test_clone_project_update_file_exists_and_differs(OSF_project, checksum):
         with patch('osfclient.cli.makedirs'):
             with patch('osfclient.cli.os.getenv', side_effect='SECRET'):
                 with patch('osfclient.cli.os.path.exists', side_effect=exists):
-                    with patch('osfclient.cli.is_folder', side_effect=_is_folder_mock):
+                    with patch('osfclient.cli.is_folder', side_effect=is_folder_mock):
                         clone(args)
 
     OSF_project.assert_called_once_with('1234')
